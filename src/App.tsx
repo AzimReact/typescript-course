1d4d3b4552e29d9abee1cc0a01c2c7cf35b556d3
Azim <azimreact@gmail.com>
2023-05-05 01:26:23 +0600

    typed response

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
-import Card, {CardVariant} from "./components/Card";
-import {IUser} from "./types/types";
+// import React from 'react';
+import { useEffect, useState } from "react";
+import Card, { CardVariant } from "./components/Card";
 import UserList from "./components/UserList";
+import { IUser } from "./types/types";
+import axios from "axios";
 
 function App() {
-    const users: IUser[] = [
-        {id: 1, name: 'Azim', email: 'azimreact@gmail.com', address: {city: 'Bishkek', street: 'Sovetskaya', zipcode: '720020'}},
-        {id: 2, name: 'Akim', email: 'akimreact@gmail.com', address: {city: 'Bishkek', street: 'Sovetskaya', zipcode: '720020'}},
-    ]
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
   return (
     <div className="App">
-        <Card onclick={(num) => console.log('void', num)} variant={CardVariant.primery} width={'200px'} height={'200px'} >
-            <button>click</button>
-            <div>qwerty</div>
-        </Card>
-        <UserList users={users} />
+      <Card
+        onclick={(num) => console.log("void", num)}
+        variant={CardVariant.primery}
+        width={"200px"}
+        height={"200px"}
+      >
+        <button>click</button>
+        <div>qwerty</div>
+      </Card>
+      <UserList users={users} />
     </div>
   );
 }
